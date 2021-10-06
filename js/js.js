6bdd0fd4592e1667d9df7c865683dccd79dddee0
front-end147 <vigo_digo@mail.ru>
2021-10-06 17:39:58 +0200

    add Barber website

---
+++ js/js.js
@@ -0,0 +1,11 @@
+const bodyTag = document.querySelector("body"),
+      menu_links = document.querySelector(".menu-links"),
+      toggleMenu = document.querySelector(".toggleMenu"),
+      menuIcon = document.querySelector(".toggleMenu .menu");
+
+toggleMenu.addEventListener("click", () => {
+  bodyTag.classList.toggle("sideMenuIsActive");
+  menu_links.classList.toggle("menuIsActive");
+  menu_links.classList.contains("menuIsActive") ? menuIcon.src = "../images/close.png" : menuIcon.src = "../images/menu.png"; ;
+  toggleMenu.classList.toggle("active");
+});
